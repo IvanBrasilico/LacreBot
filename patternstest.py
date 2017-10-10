782063cf564b5fa87dfd00dd2308f98a5e12cdfd
Unknown <brasilico.ivan@gmail.com>
2017-10-10 09:16:16 -0300

    Tests

--- patternstest.py
+++ patternstest.py
@@ -5,8 +5,11 @@ Created on Mon Oct  9 09:18:39 2017
 
 @author: ivan
 """
+from bottery.message import Message
 from patterns import FuncPattern
 from views import two_tokens, consulta_conteiner
+
+
 ptest = FuncPattern('cc', consulta_conteiner, two_tokens)
 
 print(two_tokens("duas palavras"))
